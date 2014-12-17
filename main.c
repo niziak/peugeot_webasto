fddd0533027bb594333c6fa4413b78b8a5174539
Wojciech Nizinski <niziak@spox.org>
2014-12-17 21:54:04 +0100

    first PINB0 reading

--- main.c
+++ main.c
@@ -23,18 +23,21 @@ void main(void)
     ARDUINO_LED_OFF;
 
     EventInit();
-    wdt_enable(WDTO_2S);
+    //wdt_enable(WDTO_2S);
     //TIMER_vInit()''
     //sei();
-
+    DDRB &= ~_BV(PB0); // DDR=0 = input port
+    PORTB |=  _BV(PB0); // enable pull up
 	//SimulationLoop();
 	for (;;)
 	{
-		wdt_reset();
+		//wdt_reset();
 //    		set_sleep_mode (SLEEP_MODE_IDLE); // wait for int
 //	    sleep_mode();
-	    USART0_vSendByte (USART0_ucGetByte());
-	    LOG_P(PSTR(" Woked up!\n"));
+	    //USART0_vSendByte (USART0_ucGetByte());
+		LOG_P(PSTR("PINB=0x%02X PINC=0x%02X PB0=%d\n"), PINB, PINC, PINB & _BV(PINB0));
+		_delay_ms(100);
+	    //LOG_P(PSTR(" Woked up!\n"));
 	    //_delay_ms(1000);
 	}
 }
