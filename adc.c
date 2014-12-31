5b1fe030e50bfd44da3e313dd7c5386323373afe
Wojciech Nizinski <niziak@think.spox.org>
2014-12-31 13:09:20 +0100

    universal ADC functions (selectable input and ref)

--- adc.c
+++ adc.c
@@ -5,6 +5,8 @@
  *      Author: niziak
  */
 
+#include <stdio.h>
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
@@ -12,20 +14,30 @@
 
 #include "types.h"
 #include "globals.h"
+#include "adc.h"
+#include "log.h"
+#include "usart0.h"
 
 
 ISR(ADC_vect)
 {
     // read ADCL first then ADCH
-    iTemp  = ADCL;
-	iTemp |= (ADCH<<8);
+    iADCVal  = ADCL;
+	iADCVal |= (ADCH<<8);
 }
 
-void ADC_vPrepare(void)
+
+
+/**
+ * Prepare ADC at given by @ref eChannel for conversion
+ * @param eChannel - see @ref ADC_CH_DEF
+ */
+void ADC_vPrepare (ADC_CH_DEF eChannel,  ADC_REF_DEF eReference)
 {
-    ADMUX = (   (1<<REFS1) | (1<<REFS0) /* 1.1V reference voltage for temp sensor*/
+    ADMUX = (   eReference
               | (0<<ADLAR)
-              | (1<<MUX3) );            /* 1000 - ADC8 - temp sensor */
+              | eChannel
+              );
 
     ADCSRA = (   (1<<ADEN)      /* ADC enable - set voltages*/
                | (1<<ADIE)      /* ADC INT enabled */
@@ -51,11 +63,53 @@ void ADC_vStartNoiseReduction(void)
 
 void ADC_vWait(void)
 {
-    while (bit_is_set(ADCSRA,ADSC)); // wait for end of conversion
+    while (bit_is_set  (ADCSRA,ADSC)); // wait for end of conversion
     while (bit_is_clear(ADCSRA,ADIF)); // wait for interrupt processing complete
 }
 
 void ADC_vStop(void)
 {
     ADCSRA = 0;
+}
+
+/**
+ * Measure value at given ADC channel. Result is stored in global variable @ref iADCVal
+ * Multiple ADC reads are performed and average value is given
+ * @param eChannel
+ */
+void ADC_vReadADCAverage(ADC_CH_DEF eChannel, ADC_REF_DEF eReference)
+{
+    uint16_t uiAvg=0;
+    ADC_vPrepare(eChannel, eReference);
+    for (uint8_t i=1; i<ADC_AVG_NUM_OF_SAMPLES+1; i++)
+    {
+        //DEBUG_T_P(PSTR("Start ADC%d R%02X ... "), eChannel, eReference); USART0_vFlush();
+        #if (0)
+          ADC_vStart();
+          ADC_vWait();
+        #else
+          ADC_vStartNoiseReduction();
+        #endif
+        uiAvg  = (uiAvg * (i-1)); // restore total value from previous samples
+        uiAvg += iADCVal;         // add current sample
+        uiAvg /= i;               // divide by number of current sample
+        //DEBUG_P(PSTR("ADC=%d avg=%d\n"), iADCVal, uiAvg);
+    }
+    ADC_vStop();
+}
+
+void ADC_vGetVoltage(void)
+{
+    // switch port A2/PC2 to LOW, to enable voltage divider
+    //VOLTAGE_DIVIDER_ENABLE
+    ADC_vReadADCAverage(ADC3, ADC_REF_VCC);
+    //VOLTAGE_DIVIDER_DISABLE
+
+    LOG_P(PSTR("Car voltage: (RAW=%d) "), iADCVal);
+
+//    iADCVal -= TEMP_SENS_OFFSET;
+//    iADCVal *= TEMP_SENS_GAIN_100;
+//    iADCVal /= 100;
+
+    LOG_P(PSTR("REAL=%d\n"), iADCVal);
 }
--- adc.h
+++ adc.h
@@ -8,7 +8,36 @@
 #ifndef ADC_H_
 #define ADC_H_
 
-extern void ADC_vPrepare(void);
+#include <avr/io.h>
+
+typedef enum
+{
+    ADC0 = 0,
+    ADC1,
+    ADC2,
+    ADC3,
+    ADC4,
+    ADC5,
+    ADC6,
+    ADC7,
+    ADC_TEMP,   ///< Internal temperature sensor
+    /* ... */
+    ADC_BG  = 0b1110,  ///< bandgap
+    ADC_GND = 0b1111,
+} ADC_CH_DEF;
+
+typedef enum
+{
+                                             ///< REFS1 REFS0 bits
+    ADC_REF_AREF = 0,                        ///<  00    external Aref
+    ADC_REF_VCC  = _BV(REFS0),               ///<  01    Vcc + ext cap
+    ADC_REF_INV  = _BV(REFS1),               ///<  10    invalid value
+    ADC_REF_11   = _BV(REFS1) | _BV(REFS0),  ///<  11    Internal 1.1V + ext cap
+} ADC_REF_DEF;
+
+#define ADC_AVG_NUM_OF_SAMPLES 15
+
+extern void ADC_vPrepare (ADC_CH_DEF eChannel, ADC_REF_DEF eReference);
 
 extern void ADC_vStart(void);
 extern void ADC_vStartNoiseReduction(void);
@@ -16,6 +45,10 @@ extern void ADC_vStartNoiseReduction(void);
 extern void ADC_vWait(void);
 
 extern void ADC_vStop(void);
+
+extern void ADC_vReadADCAverage(ADC_CH_DEF eChannel, ADC_REF_DEF eReference);
+
+extern void ADC_vGetVoltage(void);
 
 
 #endif /* ADC_H_ */
